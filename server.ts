d75c49dd54e450d7e855cd7bfa1c75628ad2eb80
Voinea Andrei <andrei_voinea33@yahoo.com>
2021-10-17 18:09:44 +0300

    Default server scripting

--- server.ts
+++ server.ts
@@ -1,6 +1,25 @@
 import http = require('http');
+import fs = require('fs');
+import { BetData, BetResponse, Player } from "./player.js";
+
 const port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+fs.readFile('./webpage/index.html', function (err, html) {
+    try {
+        if (err) {
+            throw err;
+        }
+    }
+    catch (err) {
+        console.log("Error while loading html");
+        return;
+    }
+
+    http.createServer(function (req, res) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(html);
+        res.end();
+
+    }).listen(port);
+
+});
